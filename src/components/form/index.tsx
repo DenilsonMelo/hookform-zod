d143de7ed0aa380b461089db42e2aa5d9e12cabb
DenilsonMelo <denilsonmlo42@gmail.com>
2025-03-15 10:36:13 -0300

    initial: codebase inicial

---
+++ src/components/form/index.tsx
@@ -0,0 +1,8 @@
+export default function Form() {
+  return (
+    <form>
+      <input type="text" className="w-32 border-2 rounded-lg"/>
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
